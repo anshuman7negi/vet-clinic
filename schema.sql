40eccf5136fd50f028e4362644df1f1f0808284b
Anshuman Negi <75941477+anshuman7negi@users.noreply.github.com>
2023-09-21 15:05:51 +0530

    Revert "Vet clinic database: create animals table"

--- schema.sql
+++ schema.sql
@@ -1,10 +1,5 @@
 /* Database schema to keep the structure of entire database. */
 
- create table animals (
- id int primary key not null,
- name varchar(20),
- date_of_birth date,
- escape_attempt int not null,
- neutered boolean,
- weight_kg decimal
- );
+CREATE TABLE animals (
+    name varchar(100)
+);
