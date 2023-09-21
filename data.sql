6ece39420b13fbd62e5a3bbc67773eaa88023f6a
anshuman7negi <anshumannegi777@gmail.com>
2023-09-21 20:38:27 +0530

    Completed task

--- data.sql
+++ data.sql
@@ -1,3 +1,32 @@
 /* Populate database with sample data. */
 
-insert into animals (id, name, date_of_birth, escape_attempt, neutered, weight_kg) values(5, 'Charmander', '2020-02-08', 0, false, -11),(6, 'Plantmon', '2021-11-15', 2, true, -5.7),(7, 'Squirtle', '1993-04-02', 3, false, -12.13),(8, 'Angemon', '2005-06-12', 1, true, -45),(9, 'Boarmon', '2005-06-07', 7, true, 20.4),(10, 'Blossom', '1998-10-13', 3, true, 17),(11, 'Ditto', '2022-05-14', 4, true, 22);
+INSERT INTO owners (full_name, age)
+VALUES
+    ('Sam Smith', 34),
+    ('Jennifer Orwell', 19),
+    ('Bob', 45),
+    ('Melody Pond', 77),
+    ('Dean Winchester', 14),
+    ('Jodie Whittaker', 38);
+
+INSERT INTO species (name)
+VALUES
+    ('Pokemon'),
+    ('Digimon');
+
+
+UPDATE animals
+SET species_id = CASE 
+    WHEN name ILIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon')
+    ELSE (SELECT id FROM species WHERE name = 'Pokemon')
+END;
+
+UPDATE animals SET owner_id = (
+  CASE
+    WHEN name = 'Agumon' THEN (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+    WHEN name IN ('Gabumon', 'Pikachu') THEN (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+    WHEN name IN ('Devimon', 'Plantmon') THEN (SELECT id FROM owners WHERE full_name = 'Bob')
+    WHEN name IN ('Charmander', 'Squirtle', 'Blossom') THEN (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+    WHEN name IN ('Angemon', 'Boarmon') THEN (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+  END
+);
--- schema.sql
+++ schema.sql
@@ -1,6 +1,20 @@
 /* Database schema to keep the structure of entire database. */
 
- alter table animals
-  add column species varchar(45);
-  ALTER TABLE
-  select * from animals;
+CREATE TABLE owners(
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(30),
+  age INTEGER
+  );
+
+   CREATE TABLE species(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100)
+ );
+
+ ALTER TABLE animals DROP COLUMN species;
+
+ ALTER TABLE animals
+ADD COLUMN species_id INTEGER REFERENCES species(id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id INTEGER REFERENCES owners(id);
