94f5155633baeed444d33ffc3bcfef49f0d4e777
anshuman7negi <anshumannegi777@gmail.com>
2023-09-21 17:54:04 +0530

    update queries

--- queries.sql
+++ queries.sql
@@ -1,10 +1,29 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals where name like '%mon';
-SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
-SELECT * FROM animals WHERE neutered = TRUE AND escape_attempt < 3;
-SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
-SELECT name, escape_attempt FROM animals WHERE weight_kg > 10.5;
-SELECT * FROM animals WHERE neutered = TRUE;
-SELECT * FROM animals WHERE name <> 'Gabumon';
-SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+delete from animals where date_of_birth > '2022-01-01';
+DELETE 1
+
+savepoint sp3;
+SAVEPOINT
+
+update animals set weight_kg = weight_kg * -1;
+UPDATE 10
+
+rollback to sp3;
+ROLLBACK
+
+update animals set weight_kg = weight_kg * -1 where weight_kg < 0;
+UPDATE 4
+commit;
+
+SELECT COUNT(*) AS total_animals FROM animals;
+
+select count(*) as animals_never_escape from animals where escape_attempts = 0;
+
+select avg(weight_kg) as average_weight from animals; 
+
+SELECT neutered, SUM(escape_attempt) AS total_escape_attempt FROM animals GROUP BY neutered ORDER BY total_escape_attempt DESC LIMIT 1;
+
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight FROM animals GROUP BY species;
+
+SELECT species, AVG(escape_attempt) AS avg_escape_attempt FROM (SELECT species, COUNT(*) AS escape_attempt FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species) AS subquery GROUP BY species;
--- schema.sql
+++ schema.sql
@@ -1,10 +1,6 @@
 /* Database schema to keep the structure of entire database. */
 
- create table animals (
- id int primary key not null,
- name varchar(20),
- date_of_birth date,
- escape_attempt int not null,
- neutered boolean,
- weight_kg decimal
- );
+ alter table animals
+  add column species varchar(45);
+  ALTER TABLE
+  select * from animals;
