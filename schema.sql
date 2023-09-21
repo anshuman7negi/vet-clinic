a9b7d4d9e92b6370fbae32c80dda4cd48a95c138
anshuman7negi <anshumannegi777@gmail.com>
2023-09-21 15:59:58 +0530

    Completed task

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+ create table animals (
+ id int primary key not null,
+ name varchar(20),
+ date_of_birth date,
+ escape_attempt int not null,
+ neutered boolean,
+ weight_kg decimal
+ );
